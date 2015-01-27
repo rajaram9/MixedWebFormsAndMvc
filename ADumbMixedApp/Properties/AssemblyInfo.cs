bff327fecfd24913f4478b368114c8bc55581c76
Kevin Jones <kevin@vcsjones.com>
2015-01-27 16:13:41 -0500

    BLARG

---
+++ ADumbMixedApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ADumbMixedApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADumbMixedApp")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("360d498c-713f-4ecc-b7f8-e0e7ff7f9158")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ADumbMixedApp/WebForms.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Web Forms" Language="C#" AutoEventWireup="true" CodeBehind="WebForms.aspx.cs" Inherits="ADumbMixedApp.WebForms" %>
+
+<p>Hi I am web web forms.</p>
+<a href="<%: GetRouteUrl(new {action = "Index", controller = "Home"}) %>">Go to MVC page.</a>
